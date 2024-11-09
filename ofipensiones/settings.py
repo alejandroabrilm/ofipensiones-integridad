1138f73fe47c5a5bae9b6d4aa8639e3c93438d5c
Fabrizio Corzo <f.corzo@uniandes.edu.co>
2024-11-08 23:34:25 -0500

    a

--- ofipensiones/settings.py
+++ ofipensiones/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'ofipensiones.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ofipensiones',
+        "USER": 'fabri',
+        "PASSWORD": 'fabri',
+        "HOST": "10.128.0.52",
+        "PORT": "",
     }
 }
 
